4db33fbb3a9d24b200eedd40b50c1e989cf20c8d
Gabriel Ford <gabe@fordltc.net>
2025-10-11 18:29:56 -0400

    Remove mod/layer taps from gaming layer

--- keymap.c
+++ keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_voyager(
     KC_EQUAL,          KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_TAB,            KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
-    LALT_T(KC_ESCAPE), KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                        KC_H,           RSFT_T(KC_J),   RCTL_T(KC_K),   RALT_T(KC_L),   RGUI_T(KC_SCLN),KC_QUOTE,
-    LCTL_T(KC_GRAVE),  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       MO(3),
-                                                                       LT(2, KC_BSPC), LSFT_T(KC_SPACE),                            KC_ENTER,       KC_SPACE
+    KC_ESCAPE,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                        KC_H,           RSFT_T(KC_J),   RCTL_T(KC_K),   RALT_T(KC_L),   RGUI_T(KC_SCLN),KC_QUOTE,
+    KC_GRAVE,          KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       MO(3),
+                                                                       KC_BSPC,        KC_SPACE,                                    LT(2, KC_ENTER),KC_SPACE
   ),
   [2] = LAYOUT_voyager(
     DF(0),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_LPRN,        KC_RPRN,        KC_TRANSPARENT,
