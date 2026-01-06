b059ecb6a3931e1c3bf67bc0781f6d269cb42ea9
Gabriel Ford <gabe@fordltc.net>
2026-01-06 16:07:44 -0500

    move mouse layer tap to enter thumb key

--- keymap.c
+++ keymap.c
@@ -26,14 +26,15 @@ enum tap_dance_codes {
 #define _BASE_LAYER 0
 #define _GAME_LAYER 1
 #define _MOUSE_LAYER 5
+#define _MLT(key) LT(_MOUSE_LAYER, key)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE_LAYER] = LAYOUT_voyager(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,               KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,               KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
-    LT(5, KC_ESC),  LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,               KC_H,           RSFT_T(KC_J),   RCTL_T(KC_K),   RALT_T(KC_L),   RGUI_T(KC_SCLN),KC_QUOTE,
+    KC_ESC,         LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,               KC_H,           RSFT_T(KC_J),   RCTL_T(KC_K),   RALT_T(KC_L),   RGUI_T(KC_SCLN),KC_QUOTE,
     KC_GRAVE,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,               KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       MO(3),
-                                                                    LT(2, KC_BSPC), LT(4, KC_TAB),      KC_ENTER,       KC_SPACE
+                                                                    LT(2, KC_BSPC), LT(4, KC_TAB),      _MLT(KC_ENTER), KC_SPACE
   ),
   // Gaming Layer - Turns off HRMods on left half.
   [_GAME_LAYER] = LAYOUT_voyager(
